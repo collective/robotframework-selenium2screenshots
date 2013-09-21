329c675a9f43fe0f8f30dc9d8c5af72e953b0f92
Asko Soukka <asko.soukka@iki.fi>
2013-09-21 16:41:58 +0300

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = u'Asko Soukka <asko.soukka@iki.fi>'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.1.1'
+release = '0.4.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
